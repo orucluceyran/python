afd4ba8890c52149dbe329e0a23f85c5b94e8c81
ders1 <orucovaceyran0306@gmail.com>
2024-05-03 10:10:50 +0400

    commit

---
+++ app.py
@@ -0,0 +1,46 @@
+#TASK-1
+# list = []
+# i=0
+# while i<5:
+#     data = int(input("Ədəd daxil edin:"))
+#     i+=1
+#     list.append(data)
+# list.sort()
+# print(list)
+
+
+
+#TASK-2
+
+# sentence = str(input("Mətn daxil edin:"))
+# sentence_sorted = ''.join(sorted(sentence))
+# print(sentence_sorted)  
+
+
+#TASK-3
+# i=0
+# while True:
+#     number = int(input("Ədəd daxil edin:"))
+#     i+=1
+#     print(f"{i}-ci cəhd: {number}")
+#     if number==13:
+#         print(f"Təbriklər! {i} cəhdə 13 ədədini tapdınız.")
+
+#TASK-4
+# for i in range(1, 100):
+#     if i > 1:
+#         for a in range(2, i):
+#             if (i % a) == 0:
+#                 break
+#         else:
+#             print(i)
+            
+            
+            
+            
+
+
+
+    
+
+      
